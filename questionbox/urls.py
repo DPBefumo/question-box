1012e8914d9669c4d8e7929f90ea4e2b5d88db31
Danny Befumo <DPBefumo@gmail.com>
2020-06-16 11:12:09 -0400

    added base and homepage template

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core_views.home_page, name='home_page'),
 ]
 
 if settings.DEBUG:
