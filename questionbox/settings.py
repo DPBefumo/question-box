639acd9e3dd0da7a831db5d742f8eb23f931561f
Danny Befumo <DPBefumo@gmail.com>
2020-06-16 21:57:00 -0400

    registration

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_extensions',
     'markdownify',
+    'registration',
 
     # Project-specific
     'users',
@@ -141,6 +142,13 @@ INTERNAL_IPS = [
     # ...
 ]
 # Configure Django App for Heroku.
+
 import django_heroku
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
+
+#django-registration-redux settings
+
+ACCOUNT_ACTIVATION_DAYS = 7
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL ='/'
